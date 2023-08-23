77ea35b62edb0e7d1c3f9281a2f4c58c669e9a79
Tobixy <shivamrajput69bs@gmail.com>
2023-08-23 06:41:56 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,4 +1,3 @@
-
 class Chat:
 
     START_TEXT = """This is a Telegram Bot to Mux subtitle into a video
@@ -35,3 +34,24 @@ Time taken : {} seconds."""
 Please provide a shorter name."""
     UNSUPPORTED_FORMAT = "ERROR : File format {} Not supported!"
     CHOOSE_CMD = "Subtitle file downloaded successfully.\nChoose your desired muxing!\n[ /softremove , /softmux , /hardmux ]"
+
+    START_TEXT_PM = """Hi there! I am a Telegram Bot to Mux subtitles into a video.
+
+You can use me to add subtitles to your videos. Just send me a video file and a subtitle file (ass or srt format), and choose the type of muxing you want.
+
+Use /help to learn more about how to use me."""
+
+    START_TEXT_GROUP = "Hi, I'm a bot to help you mux subtitles into videos. If you have any questions, send me a private message."
+
+    HELP_TEXT_PM = """<b>Welcome to the Help Menu</b>
+
+1.) Send a Video file or url.
+2.) Send a subtitle file (ass or srt)
+3.) Choose you desired type of muxing!
+
+To give custom name to file send it with url seperated with |
+<i>url|custom_name.mp4</i>
+
+<b>Note : </b><i>Please note that only english type fonts are supported in hardmux other scripts will be shown as empty blocks on the video!</i>
+
+<a href="https://github.com/mohdsabahat/sub-muxer">Repo URL</a>"""
