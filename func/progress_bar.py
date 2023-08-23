fb4866d1772c6280b747178a4370d8441c3e069f
Tobixy <shivamrajput69bs@gmail.com>
2023-08-23 07:20:21 +0530

    Update progress_bar.py

--- func/progress_bar.py
+++ func/progress_bar.py
@@ -1,54 +1,48 @@
-
 import time
 import math
 
 async def progress_bar(current, total, text, message, start):
-
     now = time.time()
-    diff = now-start
+    diff = now - start
     if round(diff % 10) == 0 or current == total:
-        percentage = current*100/total
-        speed = current/diff
-        elapsed_time = round(diff)*1000
-        eta = round((total-current)/speed)*1000
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        eta = round((total - current) / speed) * 1000
         ett = eta + elapsed_time
 
         elapsed_time = TimeFormatter(elapsed_time)
         ett = TimeFormatter(ett)
 
         progress = "[{0}{1}] \n\n🔹Progress: {2}%\n".format(
-            ''.join(["◼️" for i in range(math.floor(percentage / 5))]),
-            ''.join(["◻️" for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["◼️" for _ in range(math.floor(percentage / 5))]),
+            ''.join(["◻️" for _ in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
         tmp = progress + "{0} of {1}\n\n️🔹Speed: {2}/s\n\n🔹ETA: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
-            # elapsed_time if elapsed_time != '' else "0 s",
             ett if ett != '' else "0 s"
         )
 
-        try :
+        try:
             await message.edit(
-                text = '{}.\n{}'.format(text, tmp)
+                text='{}.\n{}'.format(text, tmp)
             )
         except:
             pass
 
 def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
     if not size:
         return ""
-    power = 2**10
+    power = 2 ** 10
     n = 0
     Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
     while size > power:
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
 
 def TimeFormatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
